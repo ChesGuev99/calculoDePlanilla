78075d3f33bd643555a58bed8c8dc6d2e169ab9b
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-04-30 13:47:08 -0600

    Procedimiento para iniciar payroll y llenar datos

---
+++ SQL Scripts/Llenar Datos.sql
@@ -0,0 +1,165 @@
+--Datos Tipos Creditos Familiares
+INSERT INTO dbo.Credit_type(type)
+VALUES ('Conyuge')
+INSERT INTO dbo.Credit_type(type)
+VALUES ('Hijos')
+
+-- Datos Tipos Cargas Sociales
+INSERT INTO dbo.Social_charges_type(ype)
+VALUES ('Obrero CCSS')
+INSERT INTO dbo.Social_charges_type(ype)
+VALUES ('Obrero LPT')
+INSERT INTO dbo.Social_charges_type(ype)
+VALUES ('Patrono CCSS')
+INSERT INTO dbo.Social_charges_type(ype)
+VALUES ('Patrono Otras Instituciones')
+INSERT INTO dbo.Social_charges_type(ype)
+VALUES ('Patrono LPT')
+
+--Datos Departamento
+insert into Department (name) values ('Recursos Humanos');
+insert into Department (name) values ('Marketing');
+insert into Department (name) values ('Ventas');
+insert into Department (name) values ('Contabilidad');
+insert into Department (name) values ('Tecnología de la Información');
+insert into Department (name) values ('Investigación y Desarrollo');
+insert into Department (name) values ('Legal');
+insert into Department (name) values ('Servicio al Cliente');
+insert into Department (name) values ('Desarrollo de Negocios');
+insert into Department (name) values ('Finanzas');
+insert into Department (name) values ('Operaciones');
+insert into Department (name) values ('Producción');
+insert into Department (name) values ('Administración');
+insert into Department (name) values ('Relaciones Públicas');
+insert into Department (name) values ('Aseguramiento de Calidad');
+insert into Department (name) values ('Compras');
+insert into Department (name) values ('Logística');
+insert into Department (name) values ('Cadena de Suministro');
+insert into Department (name) values ('Gestión de Instalaciones');
+insert into Department (name) values ('Gestión de Proyectos');
+insert into Department (name) values ('Comunicaciones');
+insert into Department (name) values ('Entrenamiento y Desarrollo');
+insert into Department (name) values ('Innovación');
+insert into Department (name) values ('Gestión de Riesgos');
+
+-- Datos Organización
+INSERT INTO dbo.Organization (name) VALUES('Google');
+INSERT INTO dbo.Organization (name) VALUES('Apple');
+INSERT INTO dbo.Organization (name) VALUES('Microsoft');
+INSERT INTO dbo.Organization (name) VALUES('Amazon');
+INSERT INTO dbo.Organization (name) VALUES('Facebook');
+INSERT INTO dbo.Organization (name) VALUES('IBM');
+INSERT INTO dbo.Organization (name) VALUES('Cisco Systems');
+INSERT INTO dbo.Organization (name) VALUES('Intel');
+INSERT INTO dbo.Organization (name) VALUES('Oracle');
+INSERT INTO dbo.Organization (name) VALUES('Dell');
+INSERT INTO dbo.Organization (name) VALUES('HP Inc.');
+INSERT INTO dbo.Organization (name) VALUES('Samsung');
+INSERT INTO dbo.Organization (name) VALUES('Sony');
+INSERT INTO dbo.Organization (name) VALUES('LG');
+INSERT INTO dbo.Organization (name) VALUES('Panasonic');
+INSERT INTO dbo.Organization (name) VALUES('Philips');
+INSERT INTO dbo.Organization (name) VALUES('Siemens');
+INSERT INTO dbo.Organization (name) VALUES('Volkswagen');
+INSERT INTO dbo.Organization (name) VALUES('Toyota');
+INSERT INTO dbo.Organization (name) VALUES('General Motors');
+INSERT INTO dbo.Organization (name) VALUES('Ford');
+INSERT INTO dbo.Organization (name) VALUES('BMW');
+INSERT INTO dbo.Organization (name) VALUES('Audi');
+INSERT INTO dbo.Organization (name) VALUES('Mercedes-Benz');
+INSERT INTO dbo.Organization (name) VALUES('Porsche');
+INSERT INTO dbo.Organization (name) VALUES('Boeing');
+INSERT INTO dbo.Organization (name) VALUES('Airbus');
+INSERT INTO dbo.Organization (name) VALUES('Lockheed Martin');
+INSERT INTO dbo.Organization (name) VALUES('Raytheon Technologies');
+INSERT INTO dbo.Organization (name) VALUES('Northrop Grumman');
+INSERT INTO dbo.Organization (name) VALUES('General Electric');
+INSERT INTO dbo.Organization (name) VALUES('Honeywell');
+INSERT INTO dbo.Organization (name) VALUES('Procter & Gamble');
+INSERT INTO dbo.Organization (name) VALUES('Unilever');
+INSERT INTO dbo.Organization (name) VALUES('Nestle');
+INSERT INTO dbo.Organization (name) VALUES('PepsiCo');
+INSERT INTO dbo.Organization (name) VALUES('Coca-Cola');
+INSERT INTO dbo.Organization (name) VALUES('Johnson & Johnson');
+INSERT INTO dbo.Organization (name) VALUES('Pfizer');
+INSERT INTO dbo.Organization (name) VALUES('Novartis');
+INSERT INTO dbo.Organization (name) VALUES('Merck');
+INSERT INTO dbo.Organization (name) VALUES('Roche');
+INSERT INTO dbo.Organization (name) VALUES('Bristol Myers Squibb');
+INSERT INTO dbo.Organization (name) VALUES('AstraZeneca');
+INSERT INTO dbo.Organization (name) VALUES('GlaxoSmithKline');
+INSERT INTO dbo.Organization (name) VALUES('ExxonMobil');
+INSERT INTO dbo.Organization (name) VALUES('Royal Dutch Shell');
+INSERT INTO dbo.Organization (name) VALUES('BP');
+INSERT INTO dbo.Organization (name) VALUES('Chevron');
+INSERT INTO dbo.Organization (name) VALUES('Total');
+INSERT INTO dbo.Organization (name) VALUES('Eni');
+INSERT INTO dbo.Organization (name) VALUES('Gazprom');
+INSERT INTO dbo.Organization (name) VALUES('Rosneft');
+INSERT INTO dbo.Organization (name) VALUES('PetroChina');
+INSERT INTO dbo.Organization (name) VALUES('Sinopec');
+INSERT INTO dbo.Organization (name) VALUES('Alibaba Group');
+INSERT INTO dbo.Organization (name) VALUES('Tencent Holdings');
+INSERT INTO dbo.Organization (name) VALUES('Baidu');
+INSERT INTO dbo.Organization (name) VALUES('JD.com');
+INSERT INTO dbo.Organization (name) VALUES('ByteDance');
+INSERT INTO dbo.Organization (name) VALUES('Huawei');
+INSERT INTO dbo.Organization (name) VALUES('Xiaomi');
+INSERT INTO dbo.Organization (name) VALUES('Nokia');
+INSERT INTO dbo.Organization (name) VALUES('AT&T');
+INSERT INTO dbo.Organization (name) VALUES('Verizon Communications');
+INSERT INTO dbo.Organization (name) VALUES('Comcast');
+INSERT INTO dbo.Organization (name) VALUES('Charter Communications');
+INSERT INTO dbo.Organization (name) VALUES('T-Mobile US');
+INSERT INTO dbo.Organization (name) VALUES('Sprint Corporation');
+INSERT INTO dbo.Organization (name) VALUES('Deutsche Telekom');
+INSERT INTO dbo.Organization (name) VALUES('Vodafone Group');
+INSERT INTO dbo.Organization (name) VALUES('Orange S.A.');
+INSERT INTO dbo.Organization (name) VALUES('Telefonica');
+INSERT INTO dbo.Organization (name) VALUES('American Express');
+INSERT INTO dbo.Organization (name) VALUES('Visa Inc.');
+INSERT INTO dbo.Organization (name) VALUES('Mastercard');
+INSERT INTO dbo.Organization (name) VALUES('PayPal');
+INSERT INTO dbo.Organization (name) VALUES('Goldman Sachs');
+INSERT INTO dbo.Organization (name) VALUES('JPMorgan Chase');
+INSERT INTO dbo.Organization (name) VALUES('Bank of America');
+INSERT INTO dbo.Organization (name) VALUES('Wells Fargo');
+INSERT INTO dbo.Organization (name) VALUES('Citigroup');
+INSERT INTO dbo.Organization (name) VALUES('Morgan Stanley');
+INSERT INTO dbo.Organization (name) VALUES('Barclays');
+INSERT INTO dbo.Organization (name) VALUES('HSBC');
+INSERT INTO dbo.Organization (name) VALUES('Deutsche Bank');
+INSERT INTO dbo.Organization (name) VALUES('Credit Suisse');
+INSERT INTO dbo.Organization (name) VALUES('UBS Group');
+INSERT INTO dbo.Organization (name) VALUES('AXA');
+INSERT INTO dbo.Organization (name) VALUES('Allianz');
+INSERT INTO dbo.Organization (name) VALUES('Prudential');
+INSERT INTO dbo.Organization (name) VALUES('MetLife');
+INSERT INTO dbo.Organization (name) VALUES('AIG');
+INSERT INTO dbo.Organization (name) VALUES('Berkshire Hathaway');
+INSERT INTO dbo.Organization (name) VALUES('Warren Buffett');
+INSERT INTO dbo.Organization (name) VALUES('BlackRock');
+INSERT INTO dbo.Organization (name) VALUES('Vanguard Group');
+INSERT INTO dbo.Organization (name) VALUES('Fidelity Investments');
+INSERT INTO dbo.Organization (name) VALUES('TELSTAR');
+
+-- Datos regulaciones
+INSERT INTO dbo.Regulation (update_date) VALUES('2022/12/31')
+
+-- Datos Impuestos
+INSERT INTO dbo.Tax_deduction (id_regulation,start_range,end_range,percentage) VALUES(1,0,941000,0)
+INSERT INTO dbo.Tax_deduction (id_regulation,start_range,end_range,percentage) VALUES(1,941000,1381000,0.1)
+INSERT INTO dbo.Tax_deduction (id_regulation,start_range,end_range,percentage) VALUES(1,1381000,2243000,0.15)
+INSERT INTO dbo.Tax_deduction (id_regulation,start_range,end_range,percentage) VALUES(1,2243000,4845000,0.2)
+INSERT INTO dbo.Tax_deduction (id_regulation,start_range,end_range,percentage) VALUES(1,4845000,50000000,0.25)
+
+-- Datos Cargas Sociales
+INSERT INTO dbo.Social_charges_deduction(id_regulation,id_type,percentage) VALUES(1,1,0.0967)
+INSERT INTO dbo.Social_charges_deduction(id_regulation,id_type,percentage) VALUES(1,2,0.01)
+INSERT INTO dbo.Social_charges_deduction(id_regulation,id_type,percentage) VALUES(1,3,0.1467)
+INSERT INTO dbo.Social_charges_deduction(id_regulation,id_type,percentage) VALUES(1,4,0.0725)
+INSERT INTO dbo.Social_charges_deduction(id_regulation,id_type,percentage) VALUES(1,5,0.0475)
+
+-- Datos Creditos Familiares
+INSERT INTO dbo.Family_credit(id_regulation,id_type,amount) VALUES(1,1,2650)
+INSERT INTO dbo.Family_credit(id_regulation,id_type,amount) VALUES(1,2,1750)
---
+++ SQL Scripts/Start_payroll.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE start_payroll(
+	@start_date datetime,
+	@end_date datetime)
+AS
+BEGIN
+	INSERT INTO  dbo.Payroll (id_regulation,start_date,end_date,total_employees,employer_deductions)
+	VALUES(1,@start_date,@end_date,0,0)
+END
