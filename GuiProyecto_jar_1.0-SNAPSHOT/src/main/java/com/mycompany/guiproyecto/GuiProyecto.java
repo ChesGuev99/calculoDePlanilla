0e4677f25220d976f77a03f9d22755010ff8b8bb
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-05-01 01:20:48 -0600

    Calcular deducciones
    Ya esta implementada la funcion que se ejecuta al presionar un boton

--- GuiProyecto_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/guiproyecto/GuiProyecto.java
+++ GuiProyecto_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/guiproyecto/GuiProyecto.java
@@ -3,7 +3,10 @@
  */
 
 package com.mycompany.guiproyecto;
+import java.io.IOException;
 import java.sql.*;
+import java.time.LocalDate;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,16 +15,77 @@ import java.util.logging.Logger;
  * @author juanp
  */
 public class GuiProyecto {
+    
+    public static void calcular_Deducciones(Connection connection) throws SQLException{
+        PreparedStatement stmt;
+        stmt = connection.prepareStatement("SELECT COUNT(*) FROM Employee");
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        int all_employees = rs.getInt(1);
+        
+        stmt = connection.prepareStatement("SELECT idn,gross_salary,kids_number,is_married FROM Employee");
+        rs = stmt.executeQuery();
+        rs.next();
+        CallableStatement proc = connection.prepareCall("{call start_payroll(?,?)}");
+        LocalDate today = LocalDate.now();
+        
+        // Obtener el primer día del mes actual
+        LocalDate firstDayOfMonth = today.withDayOfMonth(1);
+        
+        // Obtener el último día del mes actual
+        LocalDate lastDayOfMonth = today.withDayOfMonth(today.lengthOfMonth());
+        
+        // Formatear las fechas en formato aaaa/mm/dd
+        
+        
+        Date start = Date.valueOf(firstDayOfMonth);
+        Date end = Date.valueOf(lastDayOfMonth);
+        
+        proc.setDate(1,start);
+        proc.setDate(2,end);
+        
+        proc.execute();
+        
+        int batch_size = 1000;
+        
+        connection.setAutoCommit(false);
+        
+        proc = connection.prepareCall("{call calculate_deductions_by_employee(?,?,?,?,?)}");
+        
+        for (int i = 0;i < all_employees;i++){
+            proc.setInt(1,rs.getInt(1));
+            proc.setInt(2,rs.getInt(2));
+            proc.setInt(3,rs.getInt(3));
+            proc.setInt(4,rs.getInt(4));
+            proc.setInt(5,1);
+            rs.next();
+            
+            proc.addBatch();
+            
+            if (i % batch_size == 0) {
+                proc.executeBatch();
+            }  
+        }
+        
+        proc.executeBatch();
+        connection.commit();
+           
+        
+    }
+    
     public static void main(String[] args) {
         
         //Se debe modificar esto para el login correcto de la base de datos
-        String connectionUrl ="jdbc:sqlserver://DESKTOP-E9H1059;user=admin;encrypt=false";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=taxesDB;encrypt=true;trustServerCertificate=true;user=Jenaro;password=Jema0529";
         Connection connection ;
         try {
             //se conecta a la BD
             connection = DriverManager.getConnection(connectionUrl);
             System.out.println("Connection" + connection.isValid(10));
-        
+            
+            calcular_Deducciones(connection);
+            
                 //se ejecta el GUI
             StartFrame myFrame = new StartFrame(connection);
             myFrame.setVisible(true); 
