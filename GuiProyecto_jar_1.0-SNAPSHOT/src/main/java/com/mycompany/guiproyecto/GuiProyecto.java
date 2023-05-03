2f744064ce2c9f4ee440e6c4ee40e35ede0e2f35
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-05-02 19:41:34 -0600

    Nuevo main

--- GuiProyecto_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/guiproyecto/GuiProyecto.java
+++ GuiProyecto_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/guiproyecto/GuiProyecto.java
@@ -16,58 +16,6 @@ import java.util.logging.Logger;
  */
 public class GuiProyecto {
     
-    public static void calcular_Deducciones(Connection connection) throws SQLException{
-        PreparedStatement stmt;
-        stmt = connection.prepareStatement("SELECT COUNT(*) FROM Employee");
-        ResultSet rs = stmt.executeQuery();
-        rs.next();
-        int all_employees = rs.getInt(1);
-        
-        stmt = connection.prepareStatement("SELECT idn FROM Employee");
-        rs = stmt.executeQuery();
-        rs.next();
-        CallableStatement proc = connection.prepareCall("{call start_payroll(?,?)}");
-        LocalDate today = LocalDate.now();
-        
-        // Obtener el primer día del mes actual
-        LocalDate firstDayOfMonth = today.withDayOfMonth(1);
-        
-        // Obtener el último día del mes actual
-        LocalDate lastDayOfMonth = today.withDayOfMonth(today.lengthOfMonth());
-        
-        // Formatear las fechas en formato aaaa/mm/dd
-        
-        
-        Date start = Date.valueOf(firstDayOfMonth);
-        Date end = Date.valueOf(lastDayOfMonth);
-        
-        proc.setDate(1,start);
-        proc.setDate(2,end);
-        
-        proc.execute();
-        
-        int batch_size = 1000;
-        
-        connection.setAutoCommit(false);
-        
-        proc = connection.prepareCall("{call calculate_deductions_by_employee(?)}");
-        
-        for (int i = 0;i < all_employees;i++){
-            proc.setInt(1,rs.getInt(1));
-            rs.next();
-            
-            proc.addBatch();
-            
-            if (i % batch_size == 0) {
-                proc.executeBatch();
-            }  
-        }
-        
-        proc.executeBatch();
-        connection.commit();
-           
-        
-    }
     
     public static void main(String[] args) {
         
@@ -79,8 +27,7 @@ public class GuiProyecto {
             //se conecta a la BD
             connection = DriverManager.getConnection(connectionUrl);
             System.out.println("Connection" + connection.isValid(10));
-            
-            calcular_Deducciones(connection);
+           
             
                 //se ejecta el GUI
             StartFrame myFrame = new StartFrame(connection);
